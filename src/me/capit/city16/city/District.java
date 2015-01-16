06c965fa8fb52997691ab136ee3c89a0d88edcbf
Matthew Struble <matt@capit.me>
2015-01-16 06:48:42 -0800

    Fixed more conflicting files.

--- src/me/capit/city16/city/District.java
+++ src/me/capit/city16/city/District.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package me.capit.city16.city;
 
 import me.capit.eapi.data.Child;
@@ -50,48 +49,3 @@ public class District extends DataModel {
 		return getTerritory(x,z)!=null;
 	}
 }
-=======
-package me.capit.city16.city;
-
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
-
-import org.bukkit.Location;
-
-public class District implements Serializable {
-	private static final long serialVersionUID = -4858279646085128726L;
-	
-	public final UUID ID, world;
-	public final City city;
-	public final List<Territory> territory;
-	public String name;
-	
-	public District(City city, String name, UUID world){
-		ID = UUID.randomUUID();
-		this.world = world;
-		this.city = city;
-		this.name = name;
-		territory = new ArrayList<Territory>();
-	}
-	
-	public Territory getTerritory(Location loc){
-		if (!loc.getWorld().getUID().equals(world)) return null;
-		for (Territory t : territory) 
-			if (t.x==loc.getChunk().getX() && t.z==loc.getChunk().getZ()) return t;
-		return null;
-	}
-	public Territory getTerritory(UUID id){
-		for (Territory t : territory) if (t.ID.equals(id)) return t;
-		return null;
-	}
-	
-	public boolean hasTerritory(Location loc){
-		return getTerritory(loc)!=null;
-	}
-	public boolean hasTerritory(UUID id){
-		return getTerritory(id)!=null;
-	}
-}
->>>>>>> branch 'master' of https://github.com/Wehttam664/City16
--- src/me/capit/city16/city/Territory.java
+++ src/me/capit/city16/city/Territory.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package me.capit.city16.city;
 
 import me.capit.eapi.data.DataModel;
@@ -27,39 +26,3 @@ public class Territory extends DataModel {
 		return (int) ((IntValue) getAttribute("z")).getPrimitiveValue();
 	}
 }
-=======
-package me.capit.city16.city;
-
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
-
-import org.bukkit.Location;
-
-public class Territory implements Serializable {
-	private static final long serialVersionUID = -6306417376625096692L;
-	
-	public final District district;
-	public final City city;
-	public final UUID ID;
-	public final String world;
-	public final int x,z;
-	public final List<Fortification> fortifications;
-	
-	public Territory(District district, String world, int x, int z){
-		ID = UUID.randomUUID();
-		this.district = district;
-		this.city = district.city;
-		this.world = world;
-		this.x = x; this.z = z;
-		fortifications = new ArrayList<Fortification>();
-	}
-	
-	public Fortification getFortification(Location loc){
-		for (Fortification fort : fortifications) 
-			if (fort.x==loc.getBlockX() && fort.y==loc.getBlockY() && fort.z==loc.getBlockZ()) return fort;
-		return null;
-	}
-}
->>>>>>> branch 'master' of https://github.com/Wehttam664/City16
